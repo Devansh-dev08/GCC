a4783ba1566e9d4cf5d36182de0e9f941d8c15de
Dev-HCL <devansh.agarwal@hcltech.com>
2024-09-10 06:20:45 +0000

    Removed EX01 report - extra

--- ex01report/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["com/gcc/reports/ex01report/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- ex01report/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"comgccreports/ex01report/test/unit/controller/report.controller"
-], function () {
-	"use strict";
-});
